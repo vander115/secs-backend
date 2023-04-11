335334499d158c4c13b19910e257b3050204a518
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-04-11 18:04:18 -0300

    feat: add CRUD of users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -19,8 +19,8 @@ export class UsersController {
     const users = await prisma.user.create({
       data: {
         name,
-        email
-      }
+        email,
+      },
     });
 
     const listUser = await prisma.user.findMany();
@@ -33,49 +33,46 @@ export class UsersController {
   public async show(request: Request, response: Response) {
     const prisma = new PrismaClient();
 
-    const { id } = request.params; 
+    const { id } = request.params;
 
     const users = await prisma.user.findUnique({
       where: {
-        id
-      }
+        id,
+      },
     });
 
     return response.status(200).json({
       users,
     });
-  } 
+  }
 
   public async update(request: Request, response: Response) {
     const prisma = new PrismaClient();
 
-    const { id } = request.params; 
+    const { id } = request.params;
     const { name, email } = request.body;
 
     const users = await prisma.user.update({
       where: {
-        id
-      }, 
-      data: {
-        name, 
-        email
-      }
+        id,
+      },
+      data: { name, email },
     });
 
     return response.status(200).json({
       users,
     });
-  } 
+  }
 
   public async delete(request: Request, response: Response) {
     const prisma = new PrismaClient();
 
-    const { id } = request.params; 
+    const { id } = request.params;
 
     const users = await prisma.user.delete({
       where: {
         id,
-      }
+      },
     });
 
     return response.status(200).json({
